3ab0696728bd76c1ea8395c2918ba14d91427908
Ibrahim Mahdi <ibrahimmahdi115@gmail.com>
2020-01-22 10:47:54 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,37 @@
 // Code MovieReviews Here
+import React from 'react';
+
+const Review = ({
+  headline,
+  side-line,
+  link,
+  summary-short
+}) => {
+  return (
+    <div
+      key= {headline}
+      className= "review"
+    >
+      <header>
+        <a className="review-link"
+          href={link.url}
+        >
+          {headline}
+        </a>
+        <br />
+        <span className="author">
+          {side-line}
+        </span>
+        <blockquote>{summary-short}</blockquote>
+      </header>
+    </div>
+  );
+}
+
+const MovieReviews = ({ reviews }) => <div className="review-list">{reviews.map(Review)}</div>;
+
+MovieReviews.defaultProps = {
+  reviews: []
+};
+
+export default MovieReviews;
